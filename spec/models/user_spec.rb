51fb80d0d8c2655530a6babfa05ed4e802249a3d
Ahmed Taj elsir <ah.tajelsir@gmail.com>
2017-04-18 15:19:34 +0300

    [tests] add tests for User.extract_from method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -90,4 +90,45 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_presence_of(:github_url) }
     it { is_expected.to validate_presence_of(:avatar_url) }
   end
+
+  describe '.extract_from' do
+    let(:payload) do
+      # TODO: replace github_hash call with:
+      #   Faker::Omniauth.github
+      # Refer to github_hash function above for more info
+      github_hash
+    end
+
+    let(:result) do
+      User.extract_from(payload)
+    end
+
+    it 'returns valid `name`' do
+      expect(result[:name]).to eq(payload[:info][:name])
+    end
+    it 'returns valid `email`' do
+      expect(result[:email]).to eq(payload[:info][:email])
+    end
+    it 'returns valid `login`' do
+      expect(result[:login]).to eq(payload[:info][:nickname])
+    end
+    it 'returns `github_id` as instance of Integer' do
+      expect(result[:github_id]).to be_instance_of(Integer)
+    end
+    it 'returns `uid` as github_id`' do
+      expect(result[:github_id]).to eq(payload[:uid].to_i)
+    end
+    it 'returns valid `github_token`' do
+      expect(result[:github_token]).to eq(payload[:credentials][:token])
+    end
+    it 'returns valid `github_url`' do
+      expect(result[:github_url]).to eq(payload[:info][:urls][:GitHub])
+    end
+    it 'returns valid `location`' do
+      expect(result[:location]).to eq(payload[:extra][:raw_info][:location])
+    end
+    it 'returns valid `avatar_url`' do
+      expect(result[:avatar_url]).to eq(payload[:info][:image])
+    end
+  end
 end
