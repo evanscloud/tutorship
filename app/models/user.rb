baa924098f57d37a38d039f04c27b0a5f3689b53
Ahmed Taj elsir <ah.tajelsir@gmail.com>
2017-04-18 14:47:13 +0300

    [auth]  add uniqueness and presence validations to User model
    Make login, avatar_url, github_id, github_token, and github_url required.
    In addition add uniqueness constraint to login and github_id

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,31 +3,47 @@
 class User < ApplicationRecord
   devise :omniauthable, omniauth_providers: [:github]
 
+  # Required fields
+  validates :login, presence: true, uniqueness: { case_sensitive: false }
+  validates :avatar_url, presence: true
+  validates :github_id, presence: true, uniqueness: true
+  validates :github_token, presence: true
+  validates :github_url, presence: true
+
   def self.find_or_create_by(payload)
     attrs = extract_from(payload)
+
+    # Let's see if we can find an existing user
     user = User.find_by_github_id(attrs['github_id'])
+
+    # Either we have a new user
     if user.nil?
       user = User.create!(attrs)
+
+    # Or we should update our existing user's attributes
     else
       user.update_attributes(attrs)
     end
+
     user
   end
 
+  # rubocop:disable Metrics/AbcSize
   def self.extract_from(payload)
     {
-      'name'          => payload['info']['name'],
-      'email'         => payload['info']['email'],
-      'login'         => payload['info']['nickname'],
-      'github_id'     => payload['uid'].to_i,
-      'github_token'  => payload['credentials']['token'],
-      'github_url'    => payload['extra']['raw_info']['html_url'],
-      'location'      => payload['extra']['raw_info']['location'],
-      'avatar_url'    => payload['extra']['raw_info']['avatar_url']
+      name:         payload[:info][:name],
+      email:        payload[:info][:email],
+      login:        payload[:info][:nickname],
+      avatar_url:   payload[:info][:image],
+      github_id:    payload[:uid].to_i,
+      github_token: payload[:credentials][:token],
+      github_url:   payload[:extra][:raw_info][:html_url],
+      location:     payload[:extra][:raw_info][:location]
     }
   end
 
   def to_param
+    # We use `login` as an identifier in `user#show` and `user#destroy` routes
     login
   end
 end
