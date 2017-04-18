a98045b808c66ff8f3e7978e7882e424cb4b4642
Ahmed Taj elsir <ah.tajelsir@gmail.com>
2017-04-18 10:40:51 +0300

    tests: fix rspec configurations
    Remove duplicated loading of 'spce/support/*', and also the part of support/database_cleaner that causes deprecation warning.

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 RSpec.configure do |config|
-  config.before(:suite) do
-    DatabaseCleaner.clean_with(:truncation)
-  end
-
   config.before(:each) do
     DatabaseCleaner.strategy = :transaction
   end
