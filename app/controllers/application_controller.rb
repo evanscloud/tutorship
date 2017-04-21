0a6f8a72dd66b461abb31c7443b0831a15c175b9
Ahmed Taj elsir <ah.tajelsir@gmail.com>
2017-04-21 12:32:28 +0300

    [users] use 'root_path' instead of hard coded value '/' to redirect a user to after sign in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,6 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(res)
-    request.env['omniauth.origin'] || stored_location_for(res) || '/'
+    request.env['omniauth.origin'] || stored_location_for(res) || root_path
   end
 end
