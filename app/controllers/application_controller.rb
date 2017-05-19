fad5d265708860afd3abd154f217ccf5a59c67b5
Ahmed Tajelsir <ah.tajelsir@gmail.com>
2017-05-19 11:03:06 +0300

    Uses root_url instead of root_path when appropriate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def after_sign_in_path_for(_res)
-    request.env['omniauth.origin'] || root_path
+    request.env['omniauth.origin'] || root_url
   end
 
   def after_sign_out_path_for(_res)
-    root_path
+    root_url
   end
 end
